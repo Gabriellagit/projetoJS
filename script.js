a51f5de9a329b0e7e6541d3ab4987d8a67cb6d8c
Gabriellagit <104396621+Gabriellagit@users.noreply.github.com>
2022-06-01 00:39:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const frase1 = "Gabriella Ribeiro";
+const idade = 23;
+const formacao = 2018;
+const conta = 15.12;
+const boolean = true;
+
+console.log(`Meu nome é : ${frase1}, minha idade é ${idade}, ano que me formei ${formacao}, o ano e mês do meu nascimento em decimal é ${conta}` == true);
